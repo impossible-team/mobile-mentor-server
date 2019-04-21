213b6ae38f6c092592a3c1827f988dedbff355c1
Шиниев Павел <shpawel@mail.ru>
2019-04-21 05:52:43 +0300

    адрес для отметки о прохождении блока

--- apps/topic/views.py
+++ apps/topic/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
 from rest_framework import filters
+from rest_framework.decorators import action
+from rest_framework.exceptions import ParseError, NotFound
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
+from profile.models import Profile
 from .models import Topic, Block, Test
 from .serializers import TopicSerializer, TopicDetailSerializer, BlockSerializer, BlockDetailSerializer, TestSerializer, TestDetailSerializer
 
@@ -32,11 +35,26 @@ class BlockViewSet(viewsets.ModelViewSet):
 
     def initialize_request(self, request, *args, **kwargs):
         result = super(BlockViewSet, self).initialize_request(request, *args, **kwargs)
-        if self.action == 'retrieve':
+        if self.action in ['retrieve', 'studied']:
             self.serializer_class = BlockDetailSerializer
         else:
             self.serializer_class = BlockSerializer
         return result
+
+    @action(methods=['post'], detail=False, url_path='(?P<pk>[^/.]+)/studies')
+    def studied(self, request,  *args, **kwargs):
+        data = request.data
+        if 'user_id' not in data:
+            raise ParseError('Не передано обязательное значение `user_id`')
+        user_id = data['user_id']
+        try:
+            profile = Profile.objects.get(pk=user_id)
+        except Profile.DoesNotExist:
+            raise NotFound()
+        instance = self.get_object()
+        profile.blocks.add(instance)
+        serializer=self.get_serializer(instance)
+        return Response(serializer.data)
 
 
 class TestViewSet(viewsets.ModelViewSet):
