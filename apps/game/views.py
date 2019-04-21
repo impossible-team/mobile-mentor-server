97a6bb74ffe72ddf4fd84d57abf93a2297aee9ee
krasnoyarov_ai <krasnoyarov_ai@it-thematic.ru>
2019-04-21 15:07:17 +0300

    Изменено правило поределения конца игры

--- apps/game/views.py
+++ apps/game/views.py
@@ -32,23 +32,38 @@ class GameViewSet(viewsets.ModelViewSet):
         answers = {answer['test_id']: answer['answer_code'] for answer in answers}
         answers_true = {t['id']: t['correct_answer_code'] for t in
                         Test.objects.values('id', 'correct_answer_code').filter(pk__in=list(answers.keys()))}
+
         for test_id in tests_ids:
             game_result = GameResults.objects.filter(game=game, test__pk=test_id).get()
             if game.player1.pk == user_id:
                 game_result.player1 = answers_true[test_id] == answers[test_id]
-                if game.state != game.STATE_GAME_IS_ENDED:
-                    if game.state == game.STATE_SECOND_END:
-                        game.state = game.STATE_GAME_IS_ENDED
-                    else:
-                        game.state = game.STATE_FIRST_END
+                # if game.state != game.STATE_GAME_IS_ENDED:
+                #     if game.state == game.STATE_SECOND_END:
+                #         game.state = game.STATE_GAME_IS_ENDED
+                #     else:
+                #         game.state = game.STATE_FIRST_END
             elif game.player2.pk == user_id:
                 game_result.player2 = answers_true[test_id] == answers[test_id]
-                if game.state != game.STATE_GAME_IS_ENDED:
-                    if game.state == game.STATE_FIRST_END:
-                        game.state = game.STATE_GAME_IS_ENDED
-                    else:
-                        game.state = game.STATE_SECOND_END
+                # if game.state != game.STATE_GAME_IS_ENDED:
+                #     if game.state == game.STATE_FIRST_END:
+                #         game.state = game.STATE_GAME_IS_ENDED
+                #     else:
+                #         game.state = game.STATE_SECOND_END
+
             game_result.save()
+
+        if game.player1.pk == user_id:
+            if game.state == game.STATE_IN_GAME:
+                game.state = game.STATE_FIRST_END
+            else:
+                game.state = game.STATE_GAME_IS_ENDED
+
+        if game.player2.pk == user_id:
+            if game.state == game.STATE_IN_GAME:
+                game.state = game.STATE_SECOND_END
+            else:
+                game.state = game.STATE_GAME_IS_ENDED
+
         game.save()
         if game.is_ended:
             self.calculate_points()
