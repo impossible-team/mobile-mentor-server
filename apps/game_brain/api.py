540bcc4b5e0586aa2480e7e7e4584408dc204ea2
Шиняев Павел <shpawel@mail.ru>
2019-04-21 11:10:28 +0300

    добавлено заполнение таблицы с результатами игр

--- apps/game_brain/api.py
+++ apps/game_brain/api.py
@@ -1,22 +1,20 @@
+from random import shuffle
 from django.db.models import Q
 
-from profile.models import Profile
-from game.models import Game
+from game.models import Game, GameResults
 from topic.models import Test
+
 
 TESTS_COUNT = 3
 
 
 def get_game(player):
-
-
-    player_now_play = Game.objects.order_by('?').filter( Q(player1=player)| Q(player2=player), ~Q(state=Game.STATE_GAME_IS_ENDED))
+    player_now_play = Game.objects.order_by('?').filter(Q(player1=player) | Q(player2=player), ~Q(state=Game.STATE_GAME_IS_ENDED))
 
     if player_now_play:
         return player_now_play[0]
 
     # поиск случайной свободной игры
-    #!!!!!КРОМЕСЕБЯ!!!!!
     wainting_payers = Game.objects.order_by('?').filter(state__exact=Game.STATE_WAITING_FOR_PLAYERS)
     # waiter_players = Profile.objects.order_by('?').filter(state__exact=Profile.STATE_READY_TO_PLAY)
     if wainting_payers:
@@ -25,5 +23,9 @@ def get_game(player):
         g.player2 = player
         g.save()
         return g
-
-    return Game.objects.create(player1=player)
+    new_game = Game.objects.create(player1=player)
+    tests = list(Test.objects.all())
+    shuffle(tests)
+    for i in range(TESTS_COUNT):
+        GameResults.objects.create(game=new_game, test=tests[i])
+    return new_game
